761f8570971eb20bc4a0bcf065eb94c2a5a60e61
Yusuf <yusufcodes@outlook.com>
2022-06-15 10:30:50 +0100

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Yusuf Chowdhury on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
